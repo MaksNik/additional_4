fa8a98e67b9442d20905949d6dc8f36260db0544
Maksim Nikolski <nikolskijmaksim@gmail.com>
2017-09-22 02:28:28 +0300

    additional_4: implemented, all tests passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function multiply(first, second) {
-  // your solution
+    let resultArray = new Array(first.length + second.length);
+    let firstArray = first.split("");
+    let temp = second.split("").reverse();
+
+    resultArray.fill(0);
+
+    for (let i = 0; temp.length != 0; i++) {
+        let k = temp.pop();
+        for (let j = 0; j < firstArray.length; j++) {
+            resultArray[i + j] += firstArray[j] * k;
+        }
+    }
+
+    if (resultArray[resultArray.length - 1] === 0) {
+        resultArray.pop();
+    }
+
+    for (let i = resultArray.length - 1; i > 0; i--) {
+        if (Math.floor(resultArray[i] / 10) > 0) {
+            resultArray[i - 1] += Math.floor(resultArray[i] / 10);
+            resultArray[i] = resultArray[i] % 10;
+        }
+    }
+
+    return resultArray.join("");
 }
